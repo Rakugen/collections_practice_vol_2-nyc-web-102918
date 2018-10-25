980b16daac14056133e8caf290b35ba9f66b8d40
Simon Lee <simonlee0306@gmail.com>
2018-10-25 19:00:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -73,7 +73,7 @@ def merge_data(struct1, struct2)
   temp_hash = {}
   struct1.each do |key, value|
     if key.class == Hash
-      
+
     end
   end
   struct2.each do |key, value|
@@ -81,7 +81,7 @@ def merge_data(struct1, struct2)
   end
 
   temp_hash.flatten
-  
+
 end
 
 def find_cool(array)
